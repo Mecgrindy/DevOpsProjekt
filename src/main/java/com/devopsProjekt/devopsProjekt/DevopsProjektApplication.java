2f1040081fbd10fbb628a42dc7d70c92437a9b81
Your Name <you@example.com>
2017-11-30 13:26:10 +0100

    Init Projekt

---
+++ src/main/java/com/devopsProjekt/devopsProjekt/DevopsProjektApplication.java
@@ -0,0 +1,12 @@
+package com.devopsProjekt.devopsProjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsProjektApplication.class, args);
+	}
+}
